3a061764e12c66340fab76475b2785e1b56974fa
TaThuHa <thuhak2tltnpt@gmail.com>
2023-03-13 07:50:01 +0700

    up

---
+++ src/rikkei/academy/Main.java
@@ -0,0 +1,58 @@
+package rikkei.academy;
+
+import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
+
+import java.time.Instant;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	    // Kiểm thử class Shape
+//        System.out.println("----------SHAPE--------------");
+//        Shape shape = new Shape();
+//        System.out.println(shape);
+//
+//        shape = new Shape("red", false);
+//        System.out.println(shape);
+//        //Kiểm thử class Circle
+//        System.out.println("----------CIRCLE--------------");
+//        Circle circle = new Circle();
+//        System.out.println(circle);
+//        circle = new Circle(3.5);
+//        System.out.println(circle);
+//        circle = new Circle(3.5, "indigo", false);
+//        System.out.println(circle);
+//        //Kiểm thử class RECTANGLE
+//        System.out.println("---------RECTANGLE----------");
+//        Rectangle rectangle = new Rectangle();
+//        System.out.println(rectangle);
+//
+//        rectangle = new Rectangle(2.3, 5.8);
+//        System.out.println(rectangle);
+//
+//        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+//        System.out.println(rectangle);
+//        //Kiểm thử SQUARE
+//        System.out.println("---------SQUARE----------");
+//        Square square = new Square();
+//        System.out.println(square);
+//        square = new Square(2.3);
+//        System.out.println(square);
+//        square = new Square(5.8, "yellow", true);
+//        System.out.println(square);
+
+        Shape[] array = {new Rectangle(5,6,"red",true),new Square(5), new Shape() {
+            @Override
+            public double getArea() {
+                return 0;
+            }
+        },new Circle(10)};
+        for (Shape Ha: array) {
+            System.out.println( Ha.getArea());
+
+            if (Ha instanceof Colorable){
+                ((Colorable) Ha).howToColor();
+            }
+        }
+    }
+}
